251223e7627ff73b1c8cfde181eb268a9647a397
B <bcm811@gmail.com>
2016-12-14 13:55:30 -0700

    got up to iteration 407, i added [3] as way to st the counter, not required, advanced_search queries are breaking things

--- 301script.php
+++ 301script.php
@@ -17,44 +17,54 @@
 	$newArray = [];
 	foreach ($urls as $url)
 	{
-		$editUrl = 'dev.venthoodpro.com' . $url[0];
+		$editUrl = 'http://dev.venthoodpro.com' . $url[0];
 		array_push($newArray, $editUrl);
 	}
 // Set counter to have iterations of the foreach match the index of the $check_list array
-$counter = 0;
+  $array_length = count($newArray);
+  if (isset($argv[3]))
+  {
+$counter = $argv[3];;
+}
+else
+{
+  $counter = 0;
+}
 
-foreach ($newArray as $url) {
-   
+for ($counter = $counter; $counter < $array_length; $counter++) {
    $ch = curl_init();
 
    curl_setopt_array($ch, array(
-       CURLOPT_URL => $url,
+       CURLOPT_URL => $newArray[$counter],
        CURLOPT_FOLLOWLOCATION => true,
        CURLOPT_RETURNTRANSFER => true,
    ));
    $result = curl_exec($ch);
    $final = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // <-- This is the final landing point of the redirect(s)
-   echo 'another';
+   var_dump($final);
+   echo "$counter\r\n";
    while(substr($final, -1) === '/')
    {
-    echo 'running while loop 1st';
     $final = substr($final, 0,-1);
    }
    while(substr($checkArray[$counter], -1) === '/')
    {
     $checkArray[$counter] = substr($checkArray[$counter], 0, -1);
    }
+   while(substr($final, -1) === ' ')
+   {
+    array_pop($final);
+   }
    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-   $msg = "This threw a 404: " . $url;
+   $msg = "This threw a 404: " . "$newArray[$counter]\r\n";
    if($httpCode == 404) {
    	file_put_contents('404.txt', $msg, FILE_APPEND);
    }
    if (!($final === $checkArray[$counter])){
-   	$msg = 'End URL should be: ' . $checkArray[$counter] . PHP_EOL . "But is actually returning: " . $final . PHP_EOL . 'counter iteration:' . $counter . PHP_EOL . '           ' . PHP_EOL;
+   	$msg = 'URL to redirect: ' . $newArray[$counter] . PHP_EOL . 'End URL should be:|'.$checkArray[$counter].'|'. PHP_EOL . "But is actually returning:|".$final.'|'.PHP_EOL . 'counter iteration:' . $counter . PHP_EOL . '           ' . PHP_EOL;
     file_put_contents('errors.txt', $msg, FILE_APPEND) ;
    }
    curl_close($ch);
-   $counter++;
 }
 
 
